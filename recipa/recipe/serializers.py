bab4478a9dee2c26eb0d492711c3b1975cac424d
Joe Wilson <joe.wlsn@gmail.com>
2015-04-10 16:05:19 -0700

    Adds a StringRelatedField for RecipeSerializer

--- recipa/recipe/serializers.py
+++ recipa/recipe/serializers.py
@@ -1,7 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from recipe.models import Recipe
 
 
-class RecipeSerializer(ModelSerializer):
+class RecipeSerializer(serializers.ModelSerializer):
+    # set up a Nested Relationship to display both submitter name and ID
+    # http://www.django-rest-framework.org/api-guide/relations/
+    submitter = serializers.StringRelatedField()
+
     class Meta:
         model = Recipe
