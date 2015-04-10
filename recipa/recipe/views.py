3b71d172902abc0f0d472b6f219010c5f2222ed6
Joe Wilson <joe.wlsn@gmail.com>
2015-04-10 14:05:23 -0700

    Adds a view for retrieving a recipe entry

--- recipa/recipe/views.py
+++ recipa/recipe/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from recipe.models import *
+from recipe.serializers import RecipeSerializer
 
-# Create your views here.
+
+@api_view()
+def get_recipe(request, recipe_id):
+    rec = Recipe.objects.get(recipeid=recipe_id)
+    serializer = RecipeSerializer(rec, many=False)
+    return Response(serializer.data)
