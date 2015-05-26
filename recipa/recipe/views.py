2ebe88deb5c97b3882cb7958f433d367bc948760
Joe Wilson <joe.wlsn@gmail.com>
2015-05-26 11:52:07 -0700

    Removes rest-framework items from recipe views

--- recipa/recipe/views.py
+++ recipa/recipe/views.py
@@ -1,12 +1,6 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from recipe.models import *
-from recipe.serializers import RecipeSerializer
 
 
-@api_view()
 def get_recipe(request, **kwargs):
     recipe_url = kwargs.get('recipe').strip()
     rec = Recipe.objects.get(url=recipe_url)
-    serializer = RecipeSerializer(rec, many=False)
-    return Response(serializer.data)
