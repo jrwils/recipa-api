fa5c53b1f1a284473dccbe445e943f51c207bade
Joe Wilson <joe.wlsn@gmail.com>
2015-05-27 10:38:26 -0700

    Adds a API resource manager (v1) to urls.py

--- recipa/recipa/urls.py
+++ recipa/recipa/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from tastypie.api import Api
 from recipe import views as recipe_views
 from category.api import CategoryResource
+from recipe.api import RecipeResource
 
-category_resource = CategoryResource()
+
+v1_api = Api(api_name='v1')
+v1_api.register(CategoryResource())
+v1_api.register(RecipeResource())
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,5 +17,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^recipe/(?P<recipe>[-\w]+)/$', recipe_views.get_recipe, name='get_recipe'),
-    url(r'^api/', include(category_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 )
