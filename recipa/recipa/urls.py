e242f28c72f656bdf64f4cb0cc33e167a29c64d3
Joe Wilson <joe.wlsn@gmail.com>
2015-05-27 09:56:56 -0700

    Adds an API url for category resources

--- recipa/recipa/urls.py
+++ recipa/recipa/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from recipe import views as recipe_views
+from category.api import CategoryResource
+
+category_resource = CategoryResource()
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +12,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^recipe/(?P<recipe>[-\w]+)/$', recipe_views.get_recipe, name='get_recipe'),
+    url(r'^api/', include(category_resource.urls)),
 )
