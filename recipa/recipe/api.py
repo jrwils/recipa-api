22704ca0900574131e505ed2c3462acd21a90998
Joe Wilson <joe.wlsn@gmail.com>
2015-05-27 11:38:41 -0700

    Removes related category field from recipe.api.RecipeResource

--- recipa/recipe/api.py
+++ recipa/recipe/api.py
@@ -1,13 +1,10 @@
 from tastypie.authorization import DjangoAuthorization
 from tastypie.resources import ModelResource
 from tastypie import fields
-from category.api import CategoryResource
 from recipe.models import Recipe
 
 
 class RecipeResource(ModelResource):
-    category = fields.ManyToManyField(CategoryResource, 'category')
-
     class Meta:
         queryset = Recipe.objects.all()
         resource_name = 'recipe'
