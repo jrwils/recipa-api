8568583fbb206402054845391cbd87ef50c9f93a
Joe Wilson <joe.wlsn@gmail.com>
2015-05-27 11:37:23 -0700

    Adds related recipes to category.api.CategoryResource

--- recipa/category/api.py
+++ recipa/category/api.py
@@ -1,9 +1,13 @@
 from tastypie.authorization import DjangoAuthorization
 from tastypie.resources import ModelResource
+from tastypie import fields
 from category.models import Category
+from recipe.api import RecipeResource
 
 
 class CategoryResource(ModelResource):
+    recipes = fields.ManyToManyField(RecipeResource, attribute='recipe_set', null=True)
+
     class Meta:
         queryset = Category.objects.all()
         resource_name = 'category'
