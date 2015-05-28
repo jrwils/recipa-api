3f1bc98a669ce046e8605a5ff448fb7a0682f221
Joe Wilson <joe.wlsn@gmail.com>
2015-05-28 16:57:34 -0700

    Adds a dehydrate method to CategoryResource

--- recipa/category/api.py
+++ recipa/category/api.py
@@ -7,7 +7,7 @@ from recipe.api import RecipeResource
 
 
 class CategoryResource(ModelResource):
-    recipes = fields.ManyToManyField(RecipeResource, attribute='recipe_set', null=True)
+    recipes = fields.ManyToManyField(RecipeResource, attribute='recipe_set', null=True, full=True)
 
     class Meta:
         queryset = Category.objects.all()
@@ -15,3 +15,15 @@ class CategoryResource(ModelResource):
         # these should be replaced when sent to prod; only for testing
         authentication = Authentication()
         authorization = Authorization()
+        excludes = ['recipes.endcomments']
+
+    def dehydrate(self, bundle):
+        recs = bundle.data.get('recipes')
+        if recs is not None:
+            for rec in recs:
+                rec.data.pop('categories', None)
+                rec.data.pop('endcomments', None)
+                rec.data.pop('ingredients', None)
+                rec.data.pop('instructions', None)
+                rec.data.pop('intro', None)
+        return bundle
